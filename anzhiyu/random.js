e847ed3f3440461e4c56d83b9d04f4b90ade0f5f
37013658 <37013658@gitlab.com>
2026-04-05 01:11:30 +0800

    Site updated: 2026-04-05 01:11:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/04/04/hello-world/","2026/04/04/博客搭建全记录/"];function toRandomPost(){
+var posts=["2026/04/04/hello-world/","2026/04/04/博客搭建全记录/","2026/04/05/record-of-hexo-blog-repair/"];function toRandomPost(){
     pjax.loadUrl('/My_Blog/'+posts[Math.floor(Math.random() * posts.length)]);
   };
